16c68b607b35efe3220557ce98537f45423de4a5
Viktor Nyberg <viktor.nyberg00@gmail.com>
2026-04-14 17:03:40 +0200

    fix structure

--- MachineData.cs
+++
@@ -1,7 +0,0 @@
-public class MachineData
-{
-    public string MachineName { get; set; } = string.Empty;
-    public double Temperature { get; set; }
-    public bool IsRunning { get; set; }
-    public DateTime Timestamp { get; set; }
-}
